da4cbeb788793488150852bb123c06953ec09671
darshan45672 <drshnbhandary@gmail.com>
2023-05-04 20:10:31 +0530

    login to home page done

---
+++ script.js
@@ -0,0 +1,12 @@
+function auth(){
+    var email = document.getElementById("username").value;
+    var pass = document.getElementById("pass").value;
+
+    if(email == "Ajadmin@gmail.com" && pass == "admin123"){
+        window.location.assign("home.html");
+        alert("Login sucessfull!!");
+    }else{
+        alert("incorrect password or username!");
+        return;
+    }
+}
